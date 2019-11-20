15965ab032033154d2fd9d301c847ddaf272246f
bkoluch <blazej.koluch@lo1.sandomierz.plgit config --listgit config --global user.email blazej.koluch@lo1.sandomierz.pl>
2019-11-20 11:41:22 +0100

    pętla while

---
+++ python/zadanie2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def sumuj_naturalne(start,stop):
+    suma = 0 
+    liczba1 = int(input("Podaj liczbę: "))
+    liczba2 = int(input("Podaj liczbę: "))
+    for i in range(5, 11 ):
+        suma = suma + 1 
+        
+        print
+
+
+
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
