34838973fd883e1bf1b23c7a8e9228cfef0a146b
bkoluch <blazej.koluch@lo1.sandomierz.plgit config --listgit config --global user.email blazej.koluch@lo1.sandomierz.pl>
2019-11-21 12:35:36 +0100

    zadania pętla

--- python/zadanie2.py
+++ python/zadanie2.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def sumuj_naturalne(start,stop):
-    suma = 0 
-    liczba1 = int(input("Podaj liczbę: "))
-    liczba2 = int(input("Podaj liczbę: "))
-    for i in range(5, 11 ):
-        suma = suma + 1 
-        
-        print
-
-
-
-
 def main(args):
+    
+   
+    n = m = 0 
+    while n < 1:
+            start = int(input("Podaj liczbę  1 : "))
+    while m < 1 or m <= n:
+            stop = int(input("Podaj liczbę 2 : "))
+            
+    for i in range(n ,m + 1): 
+        print(i,  '', end = '')
+   
+       
+        
+        
+    
+
     return 0
 
 if __name__ == '__main__':
