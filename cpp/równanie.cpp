883a18e95cba4fe8ee304059baa87d85ea44ed19
bkoluch <blazej.koluch@lo1.sandomierz.plgit config --listgit config --global user.email blazej.koluch@lo1.sandomierz.pl>
2019-11-28 12:33:25 +0100

    równanie c++

--- cpp/równanie.cpp
+++ cpp/równanie.cpp
@@ -4,9 +4,28 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
-	
-	return 0;
+
+int main(int argc, char **argv) {
+    int a;
+    int b;
+    float x;
+    cout << "Podaj liczbę a: ";
+    cin >> a;  
+    cout << "Podaj liczbę b: ";
+    cin >> b;
+    if (a == 0) {
+        if (b == 0) {
+           cout << "Nieskończenie wiele rozwiązań "; 
+        } else {
+            cout << "Równanie sprzeczne ";
+            
+    
+        }
+    } else {
+        x = -b / (float)a;
+        cout << x;
+    }
+
+    return 0;
 }
 
