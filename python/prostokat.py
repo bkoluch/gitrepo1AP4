0b5bc371bb1f3dce99919a7e20e24e9b2d7120e8
bkoluch <blazej.koluch@lo1.sandomierz.plgit config --listgit config --global user.email blazej.koluch@lo1.sandomierz.pl>
2019-12-18 11:41:00 +0100

    zliczanie

---
+++ python/prostokat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main(args):
+
+    sz = int(input("Podaj szerokość: "))
+    wy = int(input("Podaj wysokosc: "))
+    znak = int(input("Podaj znak: "))
+    print
+
+    return 0 
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
