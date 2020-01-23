75651dd450ea34e7adbb2692b212d4b0c10aa0ea
bkoluch <blazej.koluch@lo1.sandomierz.plgit config --listgit config --global user.email blazej.koluch@lo1.sandomierz.pl>
2020-01-23 12:36:59 +0100

    kalkulator

--- python/kalkulator.py
+++ python/kalkulator.py
@@ -3,26 +3,72 @@
 #
 #  kalkulator.py
 
-wynik = 8
+wynik = 0
 
+def PobierzLiczbe():
+    liczba = int(input('Podaj liczbe:'))
+    return liczba
+    
 
+def Wynik():
+    
+    
+def Dodaj(liczba1 , liczba2)
+    global wynik
+    wynik = liczba1 + liczba2
+    
+    return wynik
+    
+    
+def Odejmij(liczba1 , liczba2)
+    global wynik
+    wynik = liczba1 - liczba2
+    
+    return wynik
+    
+def Podziel(liczba1 , liczba2)
+    global wynik
+    wynik = liczba1 / liczba2
+    
+    return  wynik
 
-operacja = int(input("Jaka operacje chcesz wykonac (
+    
+def Pomnoz(liczba1 , liczba2)
+    global wynik
+    wynik = liczba1 * liczba2
+    
+    return wynik
+    
+def WydrukujWynik():
+    
 
 def main(args):
     
-   operacja = input("Jaka operacje chcesz wykonac (+,-,/,*) ") 
-    liczba1 = int(input('Podaj licz'))
-    liczba2 = int(input("Podaj licznE"))
+operacja = input("Jaka operacje chcesz wykonac (+ , - , / , * )")
+    
+    liczba1 = PobierzLiczbe()
+    liczba2 = PobierzLiczbe()
+    wynik1 = Dodaj()
+    wynik2 = Odejmij()
+    wynik3 = Podziel()
+    wynik4 = Pomnoz()
+    
+    
+
+   
+   if operacja == '+':
+       Dodaj(liczba ,liczba2)
+    elif operacja == '-':
+        Odejmij(liczba1 ,liczba2)
+    elif operacja == '/':
+        Podziel(liczba1 ,liczba2)
+    elif operacja == '*':
+        Pomnoz(liczba ,iczba2)
+        
+   
 
     
     return 0
-    
-    
-liczba1 = int(input('Podaj licz'))
-liczba2 = int(input("Podaj licznE"))
-
-
 
 
 
