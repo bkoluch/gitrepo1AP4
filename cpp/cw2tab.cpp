51569b8d5dcf71fe94896bb1dddf022f354dfba3
bkoluch <blazej.koluch@lo1.sandomierz.plgit config --listgit config --global user.email blazej.koluch@lo1.sandomierz.pl>
2020-02-19 11:41:28 +0100

    praca na lekcji

--- cpp/cw2tab.cpp
+++ cpp/cw2tab.cpp
@@ -5,23 +5,47 @@
 
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 void wypelnij(int tab[] [10], int w, int k, int n) {
     srand(time(NULL));
-    cout << rand() % (n + 1) << endl;
+    for(int i=0; i<w; i++) {
+        for(int j=0; j<k; j++) {
+            tab[i][j] = rand() % (n + 1);
+            cout << i << "," << j << tab[i][j] << endl;
+            
+            }   
+        }
     }
 
+void sumujW(int tab[] [10], int w, int k, int n) {
+    int sumaW = 0;
+    srand(time(NULL));
+    for(int i=0; i<w; i++) {
+        for(int j=0; j<k; j++) {
+            tab[i][j] = rand() % (n + 1);
+            cout << setw(4) << tab[i][j] ;
+            sumaW += t[i][j];
+        }   
+        cout << setw(6) << endl;
+    }
+    // wydrukuj maksymalna sume oraz indeks wiersza(sumuj K) - maksymalna suma oraz indek kolumny
+}
+
+    
 int main(int argc, char **argv)
 {
-    int n = 0;
-    //cout << "Podaj wartość maksymalną: ";
-  //  cin >> n;
+    int n = 0 ;
+    cout << "Podaj wartość maksymalną: ";
+    cin >> n;
+    
     
     int w = 5;
     int k = 10;
     int tab[w] [10];
     wypelnij (tab, w, k, n);
+    sumujW (tab, w, k, n);
 	return 0;
 }
 
